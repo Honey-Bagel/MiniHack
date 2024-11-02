8dea02eabf3a330a82d6b4cd0aa6290729632bfd
Rayne Desales <rayne.desales425@gmail.com>
2024-11-01 18:18:00 -0700

    Finished working on changes

--- my-app/src/app/about/page.jsx
+++ my-app/src/app/about/page.jsx
@@ -1,9 +1,77 @@
+"use client";
+import Image from 'next/image';
+
 export default function AboutUs() {
   return (
-    <>
-      <div className="absolute bottom-0">
-        ABOUT US
+    <div className="relative bg-section h-screen">
+      {/* Background */}
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{ backgroundImage: "url('/background.jpg')" }}
+      />
+
+    {/* Opacity */}
+    <div className="absolute inset-0 bg-black opacity-5"></div>
+
+      {/* Content */}
+      <div className="relative text-9xl text-center w-full mt-20 text-black font-bold z-10">
+        Meet The Team
       </div>
-    </>
+
+      {/* Team Grid */}
+      <div className="team-grid mt-8 relative z-10 grid grid-cols-2 gap-8 max-w-md mx-auto">
+        <div className="team-member text-center">
+          <Image
+            src="/addisonfoodicon.png"
+            alt="Addison Sigsbury"
+            width={500}
+            height={150}
+            className="rounded-full"
+          />
+          <p className="text-4xl mt-2 text-black">Addison Sigsbury</p>
+        </div>
+       
+        <div className="team-member text-center">
+          <Image
+            src="/jackfoodicon.png"
+            alt="Jack Schoebel"
+            width={500}
+            height={150}
+            className="rounded-full"
+          />
+          <p className="text-4xl mt-2 text-black">Jack Schoebel</p>
+        </div>
+
+        <div className="team-member text-center">
+          <Image
+            src="/raynefoodicon.png"
+            alt="Rayne Desales"
+            width={500}
+            height={150}
+            className="rounded-full"
+          />
+          <p className="text-4xl mt-2 text-black">Rayne Desales</p>
+        </div>
+
+        <div className="team-member text-center">
+          <Image
+            src="/ericfoodicon.png"
+            alt="Eric La"
+            width={500}
+            height={150}
+            className="rounded-full"
+          />
+          <p className="text-4xl mt-2 text-black">Eric La</p>
+        </div>
+      </div>
+
+      <style jsx>{`
+        .bg-section {
+          padding: 20px;
+          text-align: center;
+          position: relative;
+        }
+      `}</style>
+    </div>
   );
 }
